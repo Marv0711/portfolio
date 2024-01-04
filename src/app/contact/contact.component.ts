5b9ad8d199ef37aeed6d05eae902eaefecaff92b
Marv0711 <marvin.buchan@t-online.de>
2024-01-04 20:51:22 +0100

    Contact works

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -19,6 +19,7 @@ export class ContactComponent {
   EmailFirstClick = false;
   MessageFirstClick = false;
   checkboxClicked = false;
+  BoxFirstClick = false
 
   isButtonDisabled = true;
 
@@ -43,53 +44,80 @@ export class ContactComponent {
 
     switch (showIcon) {
       case 'showIconName':
-        if(isInputEmpty){
-          this.showIconName = isInputEmpty;
-          this.nameFirstClick = true;
-          document.getElementById('input-name')?.classList.add('falseInput');
-          document.getElementById('input-name')?.classList.remove('rightInput');
-        }
-        else if(!isInputEmpty){
-          this.showIconName = isInputEmpty;
-          this.nameFirstClick = true;
-          document.getElementById('input-name')?.classList.remove('falseInput');
-          document.getElementById('input-name')?.classList.add('rightInput');
-        }
+        this.editNameInput(isInputEmpty);
         break;
       case 'showIconEmail':
-        if (activInputfield.value.includes('@')) {
-          let atIndex = activInputfield.value.indexOf('@');
-          this.controlAtPosition(atIndex, activInputfield);
-        }
-        else {
-          this.showIconEmail = true;
-          this.EmailFirstClick = true;
-          document.getElementById('input-mail')?.classList.add('falseInput');
-        }
+        this.editMailInput(activInputfield);
         break;
       case 'showIconMessage':
-        this.showIconMessage = isInputEmpty;
-        this.MessageFirstClick = true;
-        document.getElementById('input-message')?.classList.add('falseInput');
+        this.editMessageInput(isInputEmpty);
         break;
     }
     this.isButtonDisabled = !this.isButtonDisabled
     this.buttonActiv();
   }
 
-  controlAtPosition(atIndex:number, activInputfield:HTMLInputElement){
+  controlAtPosition(atIndex: number, activInputfield: HTMLInputElement) {
     if (atIndex < activInputfield.value.length - 1) {
       this.showIconEmail = false;
       this.EmailFirstClick = true;
-      document.getElementById('input-mail')?.classList.add('falseInput');
+      document.getElementById('input-mail')?.classList.remove('falseInput');
+      document.getElementById('input-mail')?.classList.add('rightInput');
     }
     else {
       this.showIconEmail = true;
       this.EmailFirstClick = true;
+      document.getElementById('input-mail')?.classList.remove('rightInput');
+      document.getElementById('input-mail')?.classList.add('falseInput');
+    }
+  }
+
+  editMessageInput(isInputEmpty: boolean) {
+    if (isInputEmpty) {
+      this.showIconMessage = isInputEmpty;
+      this.MessageFirstClick = true;
+      document.getElementById('input-message')?.classList.remove('rightInput');
+      document.getElementById('input-message')?.classList.add('falseInput');
+    }
+    else {
+      this.showIconMessage = isInputEmpty;
+      this.MessageFirstClick = true;
+      document.getElementById('input-message')?.classList.remove('falseInput');
+      document.getElementById('input-message')?.classList.add('rightInput');
+    }
+  }
+
+  editNameInput(isInputEmpty: boolean) {
+    if (isInputEmpty) {
+      this.showIconName = isInputEmpty;
+      this.nameFirstClick = true;
+      document.getElementById('input-name')?.classList.add('falseInput');
+      document.getElementById('input-name')?.classList.remove('rightInput');
+    }
+    else if (!isInputEmpty) {
+      this.showIconName = isInputEmpty;
+      this.nameFirstClick = true;
+      document.getElementById('input-name')?.classList.remove('falseInput');
+      document.getElementById('input-name')?.classList.add('rightInput');
+    }
+  }
+
+  editMailInput(activInputfield: HTMLInputElement) {
+    if (activInputfield.value.includes('@')) {
+      let atIndex = activInputfield.value.indexOf('@');
+      this.controlAtPosition(atIndex, activInputfield);
+    }
+    else {
+      this.showIconEmail = true;
+      this.EmailFirstClick = true;
+      document.getElementById('input-mail')?.classList.remove('rightInput');
+      document.getElementById('input-mail')?.classList.add('falseInput');
     }
   }
 
   checkBoxCklick() {
+    this.BoxFirstClick = true;
+    
     if (this.checkboxClicked) {
       this.checkboxClicked = false;
     }
@@ -98,21 +126,38 @@ export class ContactComponent {
     }
   }
 
-  swapImg(){
+  swapImg() {
     let picture = document.getElementById('arrow-up') as HTMLImageElement;
     picture.src = "assets/img/arrow-up-lineGreen.svg";
     picture.style.border = '2px solid var(--White, #70E61C)';
   }
 
-  swapImgBack(){
+  swapImgBack() {
     let picture = document.getElementById('arrow-up') as HTMLImageElement;
     picture.src = "assets/img/arrow-up-line.svg";
     picture.style.border = '2px solid var(--White, #FFF)';
   }
 
-  up(){
+  up() {
     window.scrollTo({
       top: 0,
+    });
+  }
+
+  sendMessage(event:any) {
+    debugger
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/mrgngyqo", {
+      method: "POST",
+      body: new FormData(event.target),
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(() => {
+    }).catch((error) => {
+      console.log(error);
     });
   }
 }
